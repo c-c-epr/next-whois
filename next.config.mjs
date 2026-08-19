c068e1fd8b420e3e3ab1f9ee766ca7898fd8f35b
起司玉米蛋餅 <ericxpert@gmail.com>
2026-08-19 20:28:24 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-import setupPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  turbopack: {},
+  output: "standalone",
+
   reactStrictMode: true,
-  transpilePackages: ['whoiser'],
+
+  transpilePackages: ["whoiser"],
 
   i18n: {
-    locales: ['en', 'zh', 'zh-tw', 'de', 'ru', 'ja', 'fr', 'ko'],
-    defaultLocale: 'en',
+    locales: ["en", "zh", "zh-tw", "de", "ru", "ja", "fr", "ko"],
+    defaultLocale: "en",
   },
 
   ...(process.env.NEXT_BUILD_DIR
@@ -17,25 +16,4 @@ const nextConfig = {
     : {}),
 };
 
-const withPWA = setupPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  buildExcludes: [/manifest\.json$/, /_next\/data/, /_next\/static/],
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*\.(css|js|woff2)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'assets-cache',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        },
-      },
-    },
-  ],
-});
-
-export default withPWA(nextConfig);
+export default nextConfig;
